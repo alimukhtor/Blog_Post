bea067281380258e5cf500657b18f61273d0f966
alimukhtor <james1999007@gmail.com>
2022-01-11 15:26:46 +0100

    created fully CRUD functionality for BLogPosts

--- src/services/blogs/blog.js
+++ src/services/blogs/blog.js
@@ -6,17 +6,46 @@ const blogRouter = express.Router()
 blogRouter.post("/", async(req, res, next)=> {
     try {
         const blogs = new BlogsModel(req.body)
-        console.log("Body:",req.body);
         const { _id } = await blogs.save() 
         res.status(201).send({_id})
     } catch (error) {
         next(error)
     }
 })
-blogRouter.get("/", async(req, res, next)=> {})
-blogRouter.get("/:blogId", async(req, res, next)=> {})
-blogRouter.put("/:blogId", async(req, res, next)=> {})
-blogRouter.delete("/:blogId", async(req, res, next)=> {})
+blogRouter.get("/", async(req, res, next)=> {
+    try {
+        const blogs = await BlogsModel.find()
+        res.send(blogs)
+    } catch (error) {
+        next(error)
+    }
+})
+blogRouter.get("/:blogId", async(req, res, next)=> {
+    try {
+        const blogId = await BlogsModel.findById(req.params.blogId)
+        res.send(blogId)
+    } catch (error) {
+        next(error)
+    }
+})
+blogRouter.put("/:blogId", async(req, res, next)=> {
+    try {
+        const blogId = req.params.blogId
+        const updateBlog = await BlogsModel.findByIdAndUpdate(blogId, req.body, {new:true})
+        res.send(updateBlog)
+    } catch (error) {
+        next(error)
+    }
+})
+blogRouter.delete("/:blogId", async(req, res, next)=> {
+    try {
+        const blogId = req.params.blogId
+        const deleteBlog = await BlogsModel.findByIdAndDelete(blogId)
+        res.send()
+    } catch (error) {
+        next(error)
+    }
+})
 
 
 export default blogRouter
