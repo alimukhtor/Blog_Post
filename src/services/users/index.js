0b9f21143321a4016de44dfcd9e8431d8b642b98
alimukhtor <james1999007@gmail.com>
2022-02-08 14:43:46 +0100

    created users router

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -3,10 +3,47 @@ import UserModel from './schema.js'
 
 const userRouter = express.Router()
 
-userRouter.get("/", async(req, res, next)=> {})
-userRouter.post("/", async(req, res, next)=> {})
-userRouter.get("/:userId", async(req, res, next)=> {})
-userRouter.put("/:userId", async(req, res, next)=> {})
-userRouter.delete("/:userId", async(req, res, next)=> {})
+userRouter.get("/", async(req, res, next)=> {
+    try {
+        const user = await UserModel.find()
+        res.send(user)
+    } catch (error) {
+        next(error)
+    }
+})
+userRouter.post("/", async(req, res, next)=> {
+    try {
+        const user = new UserModel(req.body)
+        const {_id} = await user.save()
+        res.send({_id})
+        console.log("Userr", user);
+    } catch (error) {
+        next(error)
+    }
+})
+userRouter.get("/:userId", async(req, res, next)=> {
+    try {
+        const user = await UserModel.findById(req.params.userId)
+        res.send(user)
+    } catch (error) {
+        next(error)
+    }
+})
+userRouter.put("/:userId", async(req, res, next)=> {
+    try {
+        const user = await UserModel.findByIdAndUpdate(req.params.userId, req.body, {new:true})
+        res.send(user)
+    } catch (error) {
+        next(error)
+    }
+})
+userRouter.delete("/:userId", async(req, res, next)=> {
+    try {
+        await UserModel.findByIdAndDelete(req.params.userId)
+        res.send()
+    } catch (error) {
+        next(error)
+    }
+})
 
 export default userRouter
--- src/services/users/schema.js
+++ src/services/users/schema.js
@@ -5,7 +5,8 @@ const {Schema, model} = mongoose
 
 const userSchema = new Schema({
     email:{type:String, required:true},
-    password:{type:String, required:true}
+    password:{type:String, required:true},
+    role:{type:String, enum:["Admin", "User"], default:"User"}
 }, {timestamps:true})
 
 export default model("User", userSchema)
